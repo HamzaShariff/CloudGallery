9b4dbdc7ec5f5f13fbdb8ea202bf7be261354705
= <hamza.sharif2002@gmail.com>
2025-07-17 19:59:11 -0400

    Add CloudFront distribution and stack outputs - create S3Origin-backed distribution for React SPA - output GalleryBucketName, CDNDomainName, CDNDistributionId

--- lib/cloud_gallery-stack.ts
+++ lib/cloud_gallery-stack.ts
@@ -1,16 +1,40 @@
-import * as cdk from 'aws-cdk-lib';
+import {Stack, StackProps, CfnOutput} from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as s3 from 'aws-cdk-lib/aws-s3';
+import * as cloudfront from 'aws-cdk-lib/aws-cloudfront';
+import * as origins from 'aws-cdk-lib/aws-cloudfront-origins';
 
-export class CloudGalleryStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+export class CloudGalleryStack extends Stack {
+  constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const galleryBucket = new s3.Bucket(this, 'GalleryBucket', {
+      versioned: false,
+      blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'CloudGalleryQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const distro = new cloudfront.Distribution(this, 'CDN', {
+      defaultBehavior: {
+        origin: new origins.S3Origin(galleryBucket),
+        compress: true,
+      },
+      defaultRootObject: 'index.html',
+    });
+
+    new CfnOutput(this, 'GalleryBucketName', {
+      value: galleryBucket.bucketName,
+      description: 'The name of the S3 bucket for images.'
+    });
+
+    new CfnOutput(this, 'CDNDomainName', {
+  value: distro.domainName,
+  description: 'CloudFront domain for the SPA',
+});
+
+new CfnOutput(this, 'CDNDistributionId', {
+  value: distro.distributionId,
+  description: 'Distribution ID (for invalidations)',
+});
+
   }
 }
