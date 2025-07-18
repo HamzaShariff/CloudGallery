db0287fde1b363c2ffb998cc8bdacbb8c6ae9179
= <hamza.sharif2002@gmail.com>
2025-07-18 15:43:15 -0400

    Add Images table and presign API - DynamoDB 'Images' table - API Gateway with CORS - Stack outputs for API URL and table name

---
+++ lambdas/api/api.py
@@ -0,0 +1,28 @@
+import boto3, os, json, uuid
+
+table = boto3.resource('dynamodb').Table(os.environ['TABLE'])
+bucket = os.environ['BUCKET']
+s3 = boto3.client('s3')
+
+def handler(event, _):
+    method = event.get('httpMethod')
+    if method == 'GET':
+        items = table.scan().get('Items', [])
+        return _resp(200, items)
+    
+    if method == 'POST':
+        image_id = str(uuid.uuid4())
+        presigned = s3.generate_presigned_url(
+            'put_object',
+            Params={'Bucket': bucket, 'Key': f"{image_id}.jpg"},
+            ExpiresIn=900
+        )
+        table.put_item(Item={'imageId': image_id, 'status': 'UPLOADING\n'})
+        return _resp(201, {'imageId': image_id, 'uploadUrl': presigned})
+    
+    return _resp(405, {'error': "Method not allowed"})
+
+def _resp(code, body):
+    return {'statusCode': code,
+            'headers': {'Content-Type': 'application/json'},
+            'body': json.dumps(body)}
--- lib/cloud_gallery-stack.ts
+++ lib/cloud_gallery-stack.ts
@@ -1,8 +1,13 @@
-import {Stack, StackProps, CfnOutput} from 'aws-cdk-lib';
+import {Stack, StackProps, CfnOutput, RemovalPolicy} from 'aws-cdk-lib';
 import { Construct } from 'constructs';
+import { Duration } from 'aws-cdk-lib';
 import * as s3 from 'aws-cdk-lib/aws-s3';
 import * as cloudfront from 'aws-cdk-lib/aws-cloudfront';
 import * as origins from 'aws-cdk-lib/aws-cloudfront-origins';
+import * as dynamodb from 'aws-cdk-lib/aws-dynamodb';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as apigw from 'aws-cdk-lib/aws-apigateway';
+import * as path from 'path';
 
 export class CloudGalleryStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
@@ -21,20 +26,60 @@ export class CloudGalleryStack extends Stack {
       defaultRootObject: 'index.html',
     });
 
+    const table = new dynamodb.Table(this, 'Images', {
+      partitionKey: {name: 'imageId', type: dynamodb.AttributeType.STRING },
+      billingMode: dynamodb.BillingMode.PAY_PER_REQUEST,
+      removalPolicy: RemovalPolicy.DESTROY
+    })
+
+    const apiFn = new lambda.Function(this, 'ApiFn', {
+      runtime: lambda.Runtime.PYTHON_3_11,
+      handler: 'api.handler',
+      code: lambda.Code.fromAsset(path.join(__dirname, '../lambdas/api')),
+      environment: {
+        TABLE: table.tableName,
+        BUCKET: galleryBucket.bucketName
+      },
+      memorySize: 128,
+      timeout: Duration.seconds(10)
+
+    });
+
+    table.grantReadWriteData(apiFn);
+    galleryBucket.grantPut(apiFn);
+
+    const api = new apigw.RestApi(this, 'GalleryApi', {
+      defaultCorsPreflightOptions: {
+        allowOrigins: apigw.Cors.ALL_ORIGINS
+      }
+    })
+
+    const images = api.root.addResource('images');
+    images.addMethod('GET', new apigw.LambdaIntegration(apiFn))
+    images.addMethod('POST', new apigw.LambdaIntegration(apiFn))
+
     new CfnOutput(this, 'GalleryBucketName', {
       value: galleryBucket.bucketName,
       description: 'The name of the S3 bucket for images.'
     });
 
     new CfnOutput(this, 'CDNDomainName', {
-  value: distro.domainName,
-  description: 'CloudFront domain for the SPA',
-});
+      value: distro.domainName,
+      description: 'CloudFront domain for the SPA',
+    });
 
-new CfnOutput(this, 'CDNDistributionId', {
-  value: distro.distributionId,
-  description: 'Distribution ID (for invalidations)',
-});
+    new CfnOutput(this, 'CDNDistributionId', {
+      value: distro.distributionId,
+      description: 'Distribution ID (for invalidations)',
+    });
+
+    new CfnOutput(this, 'ImageTablesName', {
+      value: table.tableName
+    })
+
+    new CfnOutput(this, 'GalleryApiUrl', { value: api.url });
+
+
 
   }
 }
