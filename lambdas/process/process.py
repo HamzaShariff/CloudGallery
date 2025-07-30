2db9dfff09afa06874cd99e075d06d89d3605d3d
= <hamza.sharif2002@gmail.com>
2025-07-29 21:31:47 -0400

    Convert uploads to JPEG

--- lambdas/process/process.py
+++ lambdas/process/process.py
@@ -9,29 +9,43 @@ BUCKET = os.environ['BUCKET']
 def handler(event, _):
     for rec in event['Records']:
         key = rec['s3']['object']['key']
+
         if key.startswith('thumb/'):
             continue
 
-        image_id = key.rsplit('.', 1)[0]
-        obj = s3.get_object(Bucket=BUCKET, Key=key)
-        img_bytes = obj['Body'].read()
+        image_id   = key.rsplit('.', 1)[0]
+        obj        = s3.get_object(Bucket=BUCKET, Key=key)
+        img_bytes  = obj['Body'].read()
 
-        thumb_key = f"thumb/{image_id}.jpg"
         img = Image.open(io.BytesIO(img_bytes))
-        img.thumbnail((256, 256))
-        buf = io.BytesIO()
-        img.save(buf, format='JPEG')
-        buf.seek(0)
-        s3.put_object(Bucket=BUCKET, Key=thumb_key, Body=buf, ContentType='image/jpeg')
+        rgb = img.convert('RGB')
 
-        rsp = rek.detect_labels(Image={'Bytes': img_bytes}, MaxLabels=5)
-        labels = [l['Name'] for l in rsp['Labels']]
+        thumb_key = f'thumb/{image_id}.jpg'
+        thumb_buf = io.BytesIO()
+        rgb.thumbnail((256, 256))
+        rgb.save(thumb_buf, format='JPEG', quality=85)
+        thumb_buf.seek(0)
+
+        s3.put_object(
+            Bucket      = BUCKET,
+            Key         = thumb_key,
+            Body        = thumb_buf,
+            ContentType = 'image/jpeg'
+        )
+
+        jpeg_buf = io.BytesIO()
+        rgb.save(jpeg_buf, format='JPEG', quality=90)
+        labels_rsp = rek.detect_labels(
+            Image={'Bytes': jpeg_buf.getvalue()},
+            MaxLabels=5
+        )
+        labels = [l['Name'] for l in labels_rsp['Labels']]
 
         table.update_item(
-            Key={'imageId': image_id},
-            UpdateExpression="SET #st = :r, labels = :l, thumbKey = :t",
-            ExpressionAttributeNames={'#st': 'status'},
-            ExpressionAttributeValues={
+            Key  = {'imageId': image_id},
+            UpdateExpression          = 'SET #st = :r, labels = :l, thumbKey = :t',
+            ExpressionAttributeNames  = {'#st': 'status'},
+            ExpressionAttributeValues = {
                 ':r': 'READY',
                 ':l': labels,
                 ':t': thumb_key
